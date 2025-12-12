0a0a9e9c1b15efd8eaf31818d98dec0287a15ddc
LiseHuybrecht <li.huybrecht@student.ehb.be>
2025-12-12 10:15:15 +0100

    added the code to run the server, am going to attempt to run server

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //RUN SERVER, VERY IMPORTANT
-app.listen(port, () => {
-	console.log(`app listening on port ${port}`);
-});
-
 const express = require("express");
 const fs = require("fs/promises");
 const app = express();
 const port = 3000;
+
+/*app.listen(port, () => {
+	console.log(`app listening on port ${port}`);
+});
+*/
