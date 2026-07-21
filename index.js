8cdc990824b3ccafb1755947b7af93953240a357
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-07-21 16:32:34 +0200

    render doesnt work anymore? pushing again

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.post("/addUser", async (req, res) => {
 
 	await fs.writeFile("acounts.json", JSON.stringify(data));
 
-	//res.status(201).send("upload succesful");
+	res.status(201).send("upload succesful");
 	res.send(data);
 });
 
