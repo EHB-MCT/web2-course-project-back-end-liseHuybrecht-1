4709ab0081b24e85935e40e811e684e757db0e51
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-07-25 16:17:18 +0200

    changed username

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { all } = require("axios");
 
 const password = process.env.MONGODB_PASSWORD;
 
-const uri = `mongodb+srv://lihuybrecht_db_user:${password}@cluster0.rnhnurl.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://li.huybrecht@ehb.be:${password}@cluster0.rnhnurl.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
