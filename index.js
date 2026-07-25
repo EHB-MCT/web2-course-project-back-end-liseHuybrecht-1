462ebcf3dcc3a2dc35175f9ac966f14003040385
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-07-25 17:07:43 +0200

    again

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const URI = process.env.MONGODB_URI;
 const uri = `mongodb+srv://li.huybrecht%40ehb.be:${password}@cluster0.rnhnurl.mongodb.net/?appName=Cluster0`;
 console.log(process.env.MONGODB_URI);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(URI, {
+const client = new MongoClient(uri, {
 	serverApi: {
 		version: ServerApiVersion.v1,
 		strict: true,
