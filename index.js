23d53af260032da19b0dcc300b0c97cae8c22eb9
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-07-27 05:39:48 +0200

    again

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post("/addUser", async (req, res) => {
 
 app.use(express.json());
 
-app.put("/users/:firstName", (req, res) => {
+app.put("/updateAccount/:firstName", (req, res) => {
 	const updated = replaceUser(req.params.firstName, req.body);
 	res.json(updated);
 });
