1421c603da0c7243476c494a68c80922d4385a94
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-07-28 03:51:45 +0200

    log

--- index.js
+++ index.js
@@ -196,9 +196,11 @@ app.use(express.json());
 app.patch("/updateAccountName", async (req, res) => {
 	try {
 		const db = client.db("allAcounts");
+		console.log(req.body);
+
 		const result = await db
 			.collection("acounts")
-			.updateOne({ firstName: req.query.firstName } /*{ $set: req.body }*/);
+			.updateOne({ firstName: req.query.firstName }, { $set: req.body });
 		res.json(result);
 	} catch (error) {
 		return res.status(500).json({
