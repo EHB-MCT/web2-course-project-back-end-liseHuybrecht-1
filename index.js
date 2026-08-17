1d6713048e15d2a2a636acc99b3077aabab49ec2
LiseHuybrecht <li.huybrecht@student.ehb.be>
2026-08-17 04:20:35 +0200

    duck update

--- index.js
+++ index.js
@@ -68,17 +68,17 @@ app.listen(port, () => {
 	console.log(`GET /acounts - Get all acounts`);
 	//console.log(` MONGO_URI: ${process.env.MONGODB_URI}`);
 	//console.log(process.env.YOUR_VARIABLE_NAME);
-	console.log("  ,");
-	console.log("  o>");
-	console.log("`__)");
+	console.log("   ,");
+	console.log(" ( o)>");
+	console.log("`___)");
 });
 
 app.get("/", async (req, res) => {
 	//try {
 	res.redirect("info.html");
-	console.log("  ,");
-	console.log("  o>");
-	console.log("`__)");
+	console.log("   ,");
+	console.log(" ( o)>");
+	console.log("`___)");
 
 	//const buffer = await fs.readFile("", { encode: "utf8" });
 	//} catch (error) {
